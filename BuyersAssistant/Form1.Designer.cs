96c62430734c6b6ffffc8229fdea398a551b868a
1 <1@DESKTOP-NU9I6RS>
2024-03-26 23:44:23 +0800

    BuyerAssistant Select ProductsGroup done

--- BuyersAssistant/Form1.Designer.cs
+++ BuyersAssistant/Form1.Designer.cs
@@ -28,57 +28,111 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-			this.button1 = new System.Windows.Forms.Button();
-			this.dataGridView1 = new System.Windows.Forms.DataGridView();
-			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.tabControl1 = new System.Windows.Forms.TabControl();
+			this.tabPageClients = new System.Windows.Forms.TabPage();
+			this.labelProductsCategory = new System.Windows.Forms.Label();
+			this.dgvProductsInStock = new System.Windows.Forms.DataGridView();
+			this.cbProductsGroupInStock = new System.Windows.Forms.ComboBox();
+			this.tabPageSeller = new System.Windows.Forms.TabPage();
+			this.labelProductsList = new System.Windows.Forms.Label();
+			this.tabControl1.SuspendLayout();
+			this.tabPageClients.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dgvProductsInStock)).BeginInit();
 			this.SuspendLayout();
 			// 
-			// richTextBox1
+			// tabControl1
 			// 
-			this.richTextBox1.Location = new System.Drawing.Point(12, 12);
-			this.richTextBox1.Name = "richTextBox1";
-			this.richTextBox1.Size = new System.Drawing.Size(655, 29);
-			this.richTextBox1.TabIndex = 0;
-			this.richTextBox1.Text = "";
+			this.tabControl1.Controls.Add(this.tabPageClients);
+			this.tabControl1.Controls.Add(this.tabPageSeller);
+			this.tabControl1.Location = new System.Drawing.Point(13, 13);
+			this.tabControl1.Name = "tabControl1";
+			this.tabControl1.SelectedIndex = 0;
+			this.tabControl1.Size = new System.Drawing.Size(1306, 531);
+			this.tabControl1.TabIndex = 0;
 			// 
-			// button1
+			// tabPageClients
 			// 
-			this.button1.Location = new System.Drawing.Point(694, 12);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(75, 23);
-			this.button1.TabIndex = 1;
-			this.button1.Text = "button1";
-			this.button1.UseVisualStyleBackColor = true;
+			this.tabPageClients.BackColor = System.Drawing.SystemColors.Control;
+			this.tabPageClients.Controls.Add(this.labelProductsList);
+			this.tabPageClients.Controls.Add(this.labelProductsCategory);
+			this.tabPageClients.Controls.Add(this.dgvProductsInStock);
+			this.tabPageClients.Controls.Add(this.cbProductsGroupInStock);
+			this.tabPageClients.Location = new System.Drawing.Point(4, 22);
+			this.tabPageClients.Name = "tabPageClients";
+			this.tabPageClients.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPageClients.Size = new System.Drawing.Size(1298, 505);
+			this.tabPageClients.TabIndex = 0;
+			this.tabPageClients.Text = "Client";
 			// 
-			// dataGridView1
+			// labelProductsCategory
 			// 
-			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-			this.dataGridView1.Location = new System.Drawing.Point(13, 65);
-			this.dataGridView1.Name = "dataGridView1";
-			this.dataGridView1.Size = new System.Drawing.Size(654, 309);
-			this.dataGridView1.TabIndex = 2;
+			this.labelProductsCategory.AutoSize = true;
+			this.labelProductsCategory.Location = new System.Drawing.Point(7, 5);
+			this.labelProductsCategory.Name = "labelProductsCategory";
+			this.labelProductsCategory.Size = new System.Drawing.Size(107, 13);
+			this.labelProductsCategory.TabIndex = 2;
+			this.labelProductsCategory.Text = "Категория товаров:";
+			// 
+			// dgvProductsInStock
+			// 
+			this.dgvProductsInStock.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dgvProductsInStock.Location = new System.Drawing.Point(7, 71);
+			this.dgvProductsInStock.Name = "dgvProductsInStock";
+			this.dgvProductsInStock.Size = new System.Drawing.Size(655, 414);
+			this.dgvProductsInStock.TabIndex = 1;
+			// 
+			// cbProductsGroupInStock
+			// 
+			this.cbProductsGroupInStock.FormattingEnabled = true;
+			this.cbProductsGroupInStock.Location = new System.Drawing.Point(7, 24);
+			this.cbProductsGroupInStock.Name = "cbProductsGroupInStock";
+			this.cbProductsGroupInStock.Size = new System.Drawing.Size(424, 21);
+			this.cbProductsGroupInStock.TabIndex = 0;
+			// 
+			// tabPageSeller
+			// 
+			this.tabPageSeller.BackColor = System.Drawing.SystemColors.Control;
+			this.tabPageSeller.Location = new System.Drawing.Point(4, 22);
+			this.tabPageSeller.Name = "tabPageSeller";
+			this.tabPageSeller.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPageSeller.Size = new System.Drawing.Size(1298, 505);
+			this.tabPageSeller.TabIndex = 1;
+			this.tabPageSeller.Text = "Seller";
+			// 
+			// labelProductsList
+			// 
+			this.labelProductsList.AutoSize = true;
+			this.labelProductsList.Location = new System.Drawing.Point(7, 52);
+			this.labelProductsList.Name = "labelProductsList";
+			this.labelProductsList.Size = new System.Drawing.Size(91, 13);
+			this.labelProductsList.TabIndex = 3;
+			this.labelProductsList.Text = "Список товаров:";
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(1160, 556);
-			this.Controls.Add(this.dataGridView1);
-			this.Controls.Add(this.button1);
-			this.Controls.Add(this.richTextBox1);
+			this.ClientSize = new System.Drawing.Size(1331, 556);
+			this.Controls.Add(this.tabControl1);
 			this.Name = "Form1";
 			this.Text = "Form1";
-			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.tabControl1.ResumeLayout(false);
+			this.tabPageClients.ResumeLayout(false);
+			this.tabPageClients.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dgvProductsInStock)).EndInit();
 			this.ResumeLayout(false);
 
 		}
 
 		#endregion
 
-		private System.Windows.Forms.RichTextBox richTextBox1;
-		private System.Windows.Forms.Button button1;
-		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.TabControl tabControl1;
+		private System.Windows.Forms.TabPage tabPageClients;
+		private System.Windows.Forms.DataGridView dgvProductsInStock;
+		private System.Windows.Forms.ComboBox cbProductsGroupInStock;
+		private System.Windows.Forms.TabPage tabPageSeller;
+		private System.Windows.Forms.Label labelProductsCategory;
+		private System.Windows.Forms.Label labelProductsList;
 	}
 }
 
--- BuyersAssistant/Form1.cs
+++ BuyersAssistant/Form1.cs
@@ -7,14 +7,68 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace BuyersAssistant
 {
 	public partial class Form1 : Form
 	{
+		string connectionString;
+		SqlConnection connection;
+		DataTable table;
+		SqlDataReader reader;
+
 		public Form1()
 		{
 			InitializeComponent();
+
+			connectionString = ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString;
+			connection = new SqlConnection(connectionString);
+			LoadProductsGroupToComboBox();
+			SelectAllProducts();
 		}
+		void LoadProductsGroupToComboBox()
+		{
+			string commandLine = @"SELECT products_group_name FROM ProductsGroup";
+			SqlCommand cmd = new SqlCommand(commandLine, connection);
+			connection.Open();
+			reader = cmd.ExecuteReader();
+			while(reader.Read())
+			{
+				cbProductsGroupInStock.Items.Add(reader[0]);
+			}
+			reader.Close();
+			connection.Close();
+		}
+		void SelectAllProducts()
+		{
+			string commandLine = @"
+SELECT 
+	products_name, products_group_name, quantity_in_stock, cost 
+FROM Products JOIN ProductsGroup ON Products.products_group=ProductsGroup.products_group_id";
+			SqlCommand cmd = new SqlCommand(commandLine, connection);
+			connection.Open();
+			reader = cmd.ExecuteReader();
+			table = new DataTable();
+			for(int i=0;i<reader.FieldCount;i++)
+			{
+				table.Columns.Add(reader.GetName(i));
+			}
+			while (reader.Read())
+			{
+				DataRow row = table.NewRow();
+				for(int i=0;i< reader.FieldCount;i++)
+				{
+					row[i] = reader[i];
+				}
+				table.Rows.Add(row);
+			}
+			dgvProductsInStock.DataSource = table;
+			reader.Close();
+			connection.Close();
+		}
+
 	}
 }
